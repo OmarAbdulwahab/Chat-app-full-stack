093786db857eb656bf97f93b4ac447d23fb00424
OmarAbdulwahab <omarabdulwahab982@gmail.com>
2024-06-30 19:54:33 +0300

    Pushed the Project

---
+++ server-express/index.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/signup", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+
+  // console.log("Write user into DB.");
+  // return res.json({ user: {} });
+
+  // Store a user-copy on Chat Engine!
+  try {
+    const r = await axios.post(
+      "https://api.chatengine.io/users/",
+      { username, secret, email, first_name, last_name },
+      // { headers: { "private-Key": process.env.CHAT_ENGINE_PRIVATE_KEY } }
+      { headers: { "private-Key": "dbeb3241-562a-46e5-a053-6faa8938f514" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // console.log("Fetch user from DB.");
+  // return res.json({ user: {} });
+
+  // Fetch this user from Chat Engine in this project!
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        // "Project-ID": process.env.CHAT_ENGINE_PROJECT_ID,
+        "Project-ID": "f342f534-4d71-4924-b2f7-33d75ff52ec9",
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+// Docs at rest.chatengine.io
+// vvv On port 3001!
+app.listen(3001);
